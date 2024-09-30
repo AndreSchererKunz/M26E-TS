c42a0d6f90e917af5c346bbd5ae86b1a5f2ce278
unknown <andreschererkunzt@gmail.com>
2024-09-29 22:08:19 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// função de multiplicação
+function calculaMultiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+console.log(calculaMultiplicacao(10, 2));
+// função de saudação
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(saudacao("Júlia"));
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+// função de multiplicação
+
+function calculaMultiplicacao(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+console.log(calculaMultiplicacao(10, 2));
+
+// função de saudação
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+console.log(saudacao("Júlia"));
